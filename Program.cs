bfbf90e35bcef3bb9a6c0a97d582f34b2df4ec1e
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-27 16:12:23 -0300

    deserialize musics

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
 {
     try
     {
         string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(response);
+
+        List<Music> musics = JsonSerializer.Deserialize<List<Music>>(response)!;
     }
     catch (Exception ex)
     {
