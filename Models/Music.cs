22c7ad40583019d9b4c8331999a11b09d04b3384
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-27 16:10:02 -0300

    create Music model

---
+++ Models/Music.cs
@@ -0,0 +1,17 @@
+
+using System.Text.Json.Serialization;
+
+internal class Music
+{
+    [JsonPropertyName("song")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("artist")]
+    public string? Artist { get; set; }
+
+    [JsonPropertyName("duration_ms")]
+    public int Duration { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string? Genre { get; set; }
+}
